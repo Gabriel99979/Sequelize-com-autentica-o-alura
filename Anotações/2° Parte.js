a9fb8f0520630f2ab5d494fddc6d3fe862685c99
Gabriel Henrique Oliveira Amaral <gabriel.oliveira.amaral@educacao.mg.gov.br>
2025-03-06 20:18:29 -0300

    Inicio parte 2

--- Anotações/2° Parte.js
+++ Anotações/2° Parte.js
@@ -1,1 +1,5 @@
 // Podemos dar inicio a criação do primeiro usuário
+// Vamos ter que sobrescrever o criaRegistro da classe pai e vamos ter baixar a biblioteca bcriptjs para criptografar senha e vamos usar o método hash dela
+// npm i bcrypt.js
+// Vamos na proxima aula fazer rotas privadas
+
